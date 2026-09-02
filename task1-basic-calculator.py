8dd46c777e53b2b2e63397bce43fa7e66dbbb44b
aljurcabalfin <aljur.cabalfin@ust.edu.ph>
2026-09-02 14:27:28 +0000

    feat: add formatted test results summary with f-strings

--- task1-basic-calculator.py
+++ task1-basic-calculator.py
@@ -3,3 +3,6 @@ applied_force = float(input("Enter applied force in Newtons: "))
 cross_sectional_area = float(input("Enter cross-sectional area in square meters: "))
 original_length = float(input("Enter original length in meters: "))
 change_in_length = float(input("Enter change in length in meters: "))
+
+stress = applied_force / cross_sectional_area
+strain = change_in_length / original_length
