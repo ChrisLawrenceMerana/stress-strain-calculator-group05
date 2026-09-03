3c367e2b96ffb2d94ef462e780ecea0161df0efb
Chris Lawrence <chrislawrence.merana@ust.edu.ph>
2026-09-04 01:14:57 +0800

    feat: added the data types of the material properties

--- properties.py
+++ properties.py
@@ -1,1 +1,12 @@
+"""Contains data-oriented material properties and appropriate dataclasses."""
 
+from dataclasses import asdict, dataclass
+from typing import Any, Dict
+
+@dataclass
+class MaterialProperties:
+    """Properties of a material."""
+
+    density: float  # kg/m³
+    yield_strength: float  # MPa
+    typical_youngs_modulus: float  # GPa
